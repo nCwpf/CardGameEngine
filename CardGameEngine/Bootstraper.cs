eb192bc99070413d4b698c6a9ae60ad980977dd3
nCwpf <54799864+nCwpf@users.noreply.github.com>
2019-09-05 13:07:36 +0300

    Add files via upload

---
+++ CardGameEngine/Bootstraper.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using CardGameEngine.ViewModels;
+using System.Reflection;
+
+namespace CardGameEngine
+{
+    public static class Bootstraper
+    {
+
+        public static void Init()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).Where(x => x.Name.EndsWith("ViewModel")).AsSelf();
+
+            Container = builder.Build();
+        }
+
+        public static IContainer Container { get; set; }
+
+    }
+}
